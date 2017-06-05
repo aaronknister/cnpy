17f9a8300f32bb5fc86b83e48d87a8f1ec0a7617
Aaron Knister <aaron.s.knister@nasa.gov>
2017-06-04 22:43:36 -0400

    add npz_save wrapper functions

--- cnpy_c.cpp
+++ cnpy_c.cpp
@@ -18,4 +18,20 @@ int npy_save_int32_t(const char *fname, int32_t *data, unsigned int *shape, unsi
     cnpy::npy_save(fname, data, shape, dims, mode);
 }
 
+
+
+int npz_save_int(const char *zipname, const char *fname, int *data, unsigned int *shape, unsigned int dims, const char *mode) {
+    cnpy::npz_save(zipname, fname, data, shape, dims, mode);
 }
+
+int npz_save_uint32_t(const char *zipname, const char *fname, uint32_t *data, unsigned int *shape, unsigned int dims, const char *mode) {
+    cnpy::npz_save(zipname, fname, data, shape, dims, mode);
+}
+
+int npz_save_int32_t(const char *zipname, const char *fname, int32_t *data, unsigned int *shape, unsigned int dims, const char *mode) {
+    cnpy::npz_save(zipname, fname, data, shape, dims, mode);
+}
+
+}
+
+
--- cnpy_c.h
+++ cnpy_c.h
@@ -7,6 +7,10 @@ int npy_save_int(const char *fname, int *data, unsigned int *shape, unsigned int
 int npy_save_uint32_t(const char *fname, uint32_t *data, unsigned int *shape, unsigned int dims, const char *mode);
 int npy_save_int32_t(const char *fname, int32_t *data, unsigned int *shape, unsigned int dims, const char *mode);
 
+int npz_save_int(const char *zipname, const char *fname, int *data, unsigned int *shape, unsigned int dims, const char *mode);
+int npz_save_uint32_t(const char *zipname, const char *fname, uint32_t *data, unsigned int *shape, unsigned int dims, const char *mode);
+int npz_save_int32_t(const char *zipname, const char *fname, int32_t *data, unsigned int *shape, unsigned int dims, const char *mode);
+
 #ifdef __cplusplus
 }
 #endif
